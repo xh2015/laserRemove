cb0ddd451981f9cc5fa4945e3b8d053671ee5c92
lt <lt@topsky.com>
2026-05-07 13:50:43 +0800

    引入fpv

---
+++ app/src/main/java/com/topsky/laserremove/constant/CommonData.java
@@ -0,0 +1,6 @@
+package com.topsky.laserremove.constant;
+
+public interface CommonData {
+    String RTSP_URL = "rtsp://9627b0bf2a7b.entrypoint.cloud.wowza.com:1935/app-p5260J38/66abe4b9_stream1";
+    int G20_TYPE = 7;
+}
--- app/src/main/java/com/topsky/laserremove/ui/MainActivity.java
+++ app/src/main/java/com/topsky/laserremove/ui/MainActivity.java
@@ -2,7 +2,10 @@ package com.topsky.laserremove.ui;
 
 import android.view.LayoutInflater;
 
+import com.skydroid.fpvplayer.PlayerType;
+import com.skydroid.fpvplayer.RtspTransport;
 import com.topsky.laserremove.base.BaseActivity;
+import com.topsky.laserremove.constant.CommonData;
 import com.topsky.laserremove.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
@@ -14,6 +17,22 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
     @Override
     protected void init() {
+        initFPV();
+    }
+
+    private void initFPV() {
+        //硬解码
+        binding.fpvWidget.setUsingMediaCodec(true);
+        //设置播放地址
+        binding.fpvWidget.setUrl(CommonData.RTSP_URL);
+        //使用云卓播放器
+        binding.fpvWidget.setPlayerType(PlayerType.ONLY_SKY);
+        //RTSP流TCP/UDP连接
+        binding.fpvWidget.setRtspTranstype(RtspTransport.AUTO);
+        //配置遥控器型号-可用于播放器根据遥控器型号选择比较合适的参数
+        binding.fpvWidget.setRcType(CommonData.G20_TYPE);
+
+        //binding.fpvWidget.start();
 
     }
 }
