7b568532f5f38ea17c6c8288632f83b8f5923b1a
lt <lt@topsky.com>
2026-05-07 14:47:20 +0800

    处理权限

---
+++ app/src/main/java/com/topsky/laserremove/manager/MP4RecordHelper.java
@@ -0,0 +1,222 @@
+package com.topsky.laserremove.manager;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.FileDataSourceImpl;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.tracks.h264.H264TrackImpl;
+import com.skydroid.fpvplayer.FrameInfoHelper;
+import com.skydroid.fpvplayer.SkyVideoEncoder;
+import com.skydroid.fpvplayer.VideoEncoderCallBack;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author 咔一下
+ * @date 2025/2/8 18:21
+ * @email 1501020210@qq.com
+ */
+public class MP4RecordHelper {
+
+    private int width = 1280;
+    private int height = 720;
+    private int frameRate = 15;
+    private int bitrate = 1000000;
+    private boolean useSoftEncoder = false;
+    private String h264Path;
+    private FileOutputStream h264FileOutputStream;
+
+    private SkyVideoEncoder videoEncoder;
+
+    private final String tempSuffix = ".h264";
+
+    /**
+     * 开始录像
+     * @param path
+     * @return null表示成功，!null表示失败
+     */
+    public synchronized Exception start(String path){
+        File file = new File(path + tempSuffix);
+        if (file.exists()){
+            return new IOException("File already exists:" + path);
+        }
+        try {
+            boolean retCreateNewFile = file.createNewFile();
+            if (!retCreateNewFile){
+                return new IOException("createNewFile fail:" + path);
+            }
+            this.h264Path = file.getAbsolutePath();
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            SkyVideoEncoder videoEncoder = new SkyVideoEncoder();
+            videoEncoder.setVideoEncoderCallBack(new VideoEncoderCallBack() {
+                byte[] sps;
+                byte[] pps;
+                boolean isWaitSPSPPS = true;
+                @Override
+                public void onVideoEncodeData(int frameType, byte[] frame, long timestamp_us) {
+                    switch (frameType){
+                        case SkyVideoEncoder.H264_SPS:
+                            sps = new byte[frame.length];
+                            System.arraycopy(frame,0,sps,0,frame.length);
+                            break;
+                        case SkyVideoEncoder.H264_PPS:
+                            pps = new byte[frame.length];
+                            System.arraycopy(frame,0,pps,0,frame.length);
+                            if (isWaitSPSPPS){
+                                isWaitSPSPPS = false;
+                            }
+                            try {
+                                fileOutputStream.write(sps);
+                                fileOutputStream.write(pps);
+                                fileOutputStream.flush();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            break;
+                        case SkyVideoEncoder.H264_IDR:
+                            if (isWaitSPSPPS){
+                                break;
+                            }
+                            byte[][] datas = FrameInfoHelper.splitH264NALU(frame);
+                            try {
+                                if (datas == null){
+                                    fileOutputStream.write(frame);
+                                }else{
+                                    fileOutputStream.write(datas[2]);
+                                }
+                                fileOutputStream.flush();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            break;
+                        case SkyVideoEncoder.H264_FRAME:
+                            if (isWaitSPSPPS){
+                                break;
+                            }
+                            try {
+                                fileOutputStream.write(frame);
+                                fileOutputStream.flush();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            break;
+                    }
+                }
+            });
+            this.videoEncoder = videoEncoder;
+            this.h264FileOutputStream = fileOutputStream;
+            videoEncoder.start(width,height,frameRate,bitrate,useSoftEncoder);
+        } catch (Exception e) {
+            return e;
+        }
+        return null;
+    }
+
+    public synchronized void stop(final MP4RecordCallBack callBack){
+        final String h264Path = this.h264Path;
+        final SkyVideoEncoder videoEncoder = this.videoEncoder;
+        if (videoEncoder == null){
+            if (callBack != null){
+                callBack.onRecordComplete(new IllegalStateException("Not started"));
+            }
+            return;
+        }
+        final FileOutputStream h264FileOutputStream = this.h264FileOutputStream;
+        videoEncoder.stop();
+        this.videoEncoder = null;
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    if (h264FileOutputStream != null){
+                        h264FileOutputStream.flush();
+                        h264FileOutputStream.close();
+                    }
+                    H264TrackImpl h264Track = new H264TrackImpl(new FileDataSourceImpl(h264Path));
+                    Movie movie = new Movie();
+                    movie.addTrack(h264Track);
+                    Container mp4file = new DefaultMp4Builder().build(movie);
+                    FileChannel fc = new FileOutputStream(new File(h264Path.substring(0,h264Path.length() - tempSuffix.length()))).getChannel();
+                    mp4file.writeContainer(fc);
+                    fc.close();
+                    boolean delRet = new File(h264Path).delete();
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (callBack != null){
+                                callBack.onRecordComplete(null);
+                            }
+                        }
+                    });
+                }catch (Exception e){
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (callBack != null){
+                                callBack.onRecordComplete(e);
+                            }
+                        }
+                    });
+                }
+
+            }
+        }.start();
+
+    }
+
+    public void putYUV(ByteBuffer directBuffer, int width, int height, int pixel_format){
+        SkyVideoEncoder videoEncoder = this.videoEncoder;
+        if (videoEncoder != null){
+            videoEncoder.putYUV(directBuffer,width,height,pixel_format);
+        }
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setVideoSize(int width,int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getFrameRate() {
+        return frameRate;
+    }
+
+    public void setFrameRate(int frameRate) {
+        this.frameRate = frameRate;
+    }
+
+    public int getBitrate() {
+        return bitrate;
+    }
+
+    public void setBitrate(int bitrate) {
+        this.bitrate = bitrate;
+    }
+
+    public boolean isUseSoftEncoder() {
+        return useSoftEncoder;
+    }
+
+    public void setUseSoftEncoder(boolean useSoftEncoder) {
+        this.useSoftEncoder = useSoftEncoder;
+    }
+
+    public static interface MP4RecordCallBack{
+        public void onRecordComplete(Exception e);
+    }
+}
--- app/src/main/java/com/topsky/laserremove/ui/MainActivity.java
+++ app/src/main/java/com/topsky/laserremove/ui/MainActivity.java
@@ -1,14 +1,27 @@
 package com.topsky.laserremove.ui;
 
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
 
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.PathUtils;
+import com.hjq.permissions.XXPermissions;
+import com.hjq.permissions.permission.PermissionLists;
 import com.skydroid.fpvplayer.PlayerType;
 import com.skydroid.fpvplayer.RtspTransport;
+import com.skydroid.fpvplayer.VideoDecoderCallBack;
+import com.topsky.laserremove.R;
 import com.topsky.laserremove.base.BaseActivity;
 import com.topsky.laserremove.constant.CommonData;
 import com.topsky.laserremove.databinding.ActivityMainBinding;
+import com.topsky.laserremove.manager.MP4RecordHelper;
 
-public class MainActivity extends BaseActivity<ActivityMainBinding> {
+import java.nio.ByteBuffer;
+
+import androidx.annotation.NonNull;
+
+public class MainActivity extends BaseActivity<ActivityMainBinding> implements View.OnClickListener, VideoDecoderCallBack, MP4RecordHelper.MP4RecordCallBack {
 
     @Override
     protected ActivityMainBinding initViewBinding(LayoutInflater inflater) {
@@ -18,6 +31,12 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     @Override
     protected void init() {
         initFPV();
+        initView();
+        initRecord();
+    }
+
+    private void initView() {
+        binding.btnRecord.setOnClickListener(this);
     }
 
     private void initFPV() {
@@ -32,7 +51,99 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
         //配置遥控器型号-可用于播放器根据遥控器型号选择比较合适的参数
         binding.fpvWidget.setRcType(CommonData.G20_TYPE);
 
+        binding.fpvWidget.setVideoDecoderCallBack(this);
+
         //binding.fpvWidget.start();
 
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_record) {
+            //录屏
+            recordOpt();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        binding.fpvWidget.stop();
+        if (isStartRecord && mp4RecordHelper != null) {
+            mp4RecordHelper.stop(this);
+        }
+    }
+
+    //region 录屏
+    private MP4RecordHelper mp4RecordHelper;
+    private boolean isStartRecord = false;
+
+    private void initRecord() {
+        mp4RecordHelper = new MP4RecordHelper();
+    }
+
+    private void recordOpt() {
+        if (isStartRecord) {
+            stopRecord();
+        } else {
+            if (XXPermissions.isGrantedPermission(this, PermissionLists.getManageExternalStoragePermission())) {
+                startRecord();
+            } else {
+                XXPermissions.with(this)
+                        .permission(PermissionLists.getManageExternalStoragePermission())
+                        .request((grantedList, deniedList) -> {
+                        });
+            }
+        }
+    }
+
+    private void startRecord() {
+        if (mp4RecordHelper == null) {
+            return;
+        }
+        mp4RecordHelper.setBitrate(1500000);//比特率
+        //这里最好不能超过30帧，编码的速度没那么快，可能会导致录制时间不符合预期
+        mp4RecordHelper.setFrameRate(15);//输出帧率
+        mp4RecordHelper.setVideoSize(1280, 720); //输出分辨率
+        mp4RecordHelper.setUseSoftEncoder(true);//true:软编码（X264），false:硬编码
+        String path = PathUtils.getExternalDownloadsPath() + "/" + System.currentTimeMillis() + ".mp4";
+        Exception ret = mp4RecordHelper.start(path);
+        if (ret == null) {
+            isStartRecord = true;
+            switchRecordText();
+        } else {
+            LogUtils.e("录像失败：" + ret);
+        }
+    }
+
+    private void stopRecord() {
+        if (mp4RecordHelper != null) {
+            mp4RecordHelper.stop(this);
+        }
+    }
+
+    @Override
+    public void onYUV(@NonNull ByteBuffer data, int width, int height, int pixel_format) {
+        if (mp4RecordHelper != null) {
+            mp4RecordHelper.putYUV(data, width, height, pixel_format);
+        }
+    }
+
+    @Override
+    public void onRecordComplete(Exception e) {
+        if (e != null) {
+            LogUtils.e("停止录像失败：" + e);
+        }
+        isStartRecord = false;
+        switchRecordText();
+    }
+
+    private void switchRecordText() {
+        if (isStartRecord) {
+            binding.btnRecord.setText("停止录像");
+        } else {
+            binding.btnRecord.setText("开始录像");
+        }
+    }
+//endregion
 }
